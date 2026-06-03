033024b484e757b669b9684cc5fb3f05d453996a
Lucas Arroyo <elpropiolucas@gmail.com>
2026-06-03 17:51:14 +0200

    Nav

---
+++ wwwroot/js/section-observer.js
@@ -0,0 +1,30 @@
+﻿window.sectionObserver = {
+
+    observeSections: () => {
+
+        const sections = document.querySelectorAll("section[id]");
+
+        const observer = new IntersectionObserver(entries => {
+
+            entries.forEach(entry => {
+
+                if (!entry.isIntersecting)
+                    return;
+
+                const id = entry.target.id;
+
+                history.replaceState(
+                    null,
+                    "",
+                    `${window.location.pathname}#${id}`
+                );
+
+            });
+
+        }, {
+            threshold: 0.5
+        });
+
+        sections.forEach(section => observer.observe(section));
+    }
+};
