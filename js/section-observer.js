c51b34e6e36775b41f17b90180d70ff861889b0c
l-arroyo <113597037+l-arroyo@users.noreply.github.com>
2026-06-04 18:24:46 +0000

    Deploying to gh-pages from @ l-arroyo/l-arroyo.github.io@62629160c154e49d961902f6ed4e46d9485db472 🚀

---
+++ js/section-observer.js
@@ -0,0 +1,31 @@
+﻿window.sectionObserver = {
+
+    init: (dotnetHelper) => {
+
+        const sections = document.querySelectorAll("section[id]");
+
+        const observer = new IntersectionObserver((entries) => {
+
+            let visible = entries
+                .filter(e => e.isIntersecting)
+                .sort((a, b) => b.intersectionRatio - a.intersectionRatio)[0];
+
+            if (!visible) return;
+
+            const id = visible.target.id;
+
+            dotnetHelper.invokeMethodAsync("ShouldIgnoreObserver")
+                .then(ignore => {
+
+                    if (ignore) return;
+
+                    dotnetHelper.invokeMethodAsync("SetActiveSection", id);
+                });
+
+        }, {
+            threshold: [0.3, 0.5, 0.7]
+        });
+
+        sections.forEach(s => observer.observe(s));
+    }
+};
---
+++ js/theme-handler.js
@@ -0,0 +1,14 @@
+﻿function setTheme(themeName) {
+
+    //add a new css link
+    let newLink = document.createElement("link");
+    newLink.setAttribute("id", "theme");
+    newLink.setAttribute("rel", "stylesheet");
+    newLink.setAttribute("type", "text/css");
+    newLink.setAttribute("href", `css/themes/${themeName}-theme.css`);
+    //remove the theme from the head tag
+    let head = document.getElementsByTagName("head")[0];
+    head.querySelector("#theme").remove();
+    //adding newLink in the head
+    head.appendChild(newLink);
+}
