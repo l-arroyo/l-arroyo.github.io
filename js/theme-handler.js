c2e6518d952662d9b03faa0926c906468a9ca374
l-arroyo <113597037+l-arroyo@users.noreply.github.com>
2026-06-07 10:25:26 +0000

    Deploying to gh-pages from @ l-arroyo/l-arroyo.github.io@f60c8124c59220a35e364ababbabea1c0f2fadac 🚀

--- js/theme-handler.js
+++ js/theme-handler.js
@@ -1,14 +1,27 @@
-﻿function setTheme(themeName) {
+function setTheme(themeName) {
 
-    //add a new css link
-    let newLink = document.createElement("link");
-    newLink.setAttribute("id", "theme");
+    const currentLink = document.querySelector("#theme");
+    const nextHref = `css/themes/${themeName}-theme.css`;
+
+    if (currentLink?.getAttribute("href") === nextHref)
+        return;
+
+    const newLink = document.createElement("link");
+
     newLink.setAttribute("rel", "stylesheet");
     newLink.setAttribute("type", "text/css");
-    newLink.setAttribute("href", `css/themes/${themeName}-theme.css`);
-    //remove the theme from the head tag
-    let head = document.getElementsByTagName("head")[0];
-    head.querySelector("#theme").remove();
-    //adding newLink in the head
-    head.appendChild(newLink);
+    newLink.setAttribute("href", nextHref);
+
+    if (!currentLink) {
+        newLink.setAttribute("id", "theme");
+        document.head.appendChild(newLink);
+        return;
+    }
+
+    newLink.onload = () => {
+        currentLink.remove();
+        newLink.setAttribute("id", "theme");
+    };
+
+    document.head.appendChild(newLink);
 }
